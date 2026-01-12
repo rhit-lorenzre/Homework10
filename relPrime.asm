033b829294d5cc891f6569ed80e0bdb2c4b8d2c1
rhit-lorenzre <lorenzre@rose-hulman.edu>
2026-01-12 16:50:13 -0500

    relPrime and gcd passing

--- relPrime.asm
+++ relPrime.asm
@@ -31,34 +31,55 @@ jal x0, runtests
 # PROCEDURE - gcd(a, b)
 ##############################################################
 gcd:
-    addi sp, sp, -8
-    sw ra, 0(sp) 
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw a0, 4(sp) #a0 = a
+    sw a1, 8(sp) #a1 = b
 
-    sw a0, 0(sp) #a0 = a
-    sw a1, 4(sp) #a1 = b
+    beq a0, x0, RETURN_B
 
-    bne a0, 0, ELSE
-
-    addi a0, a1, 0 #return b
-
-ELSE:
-
-    beq a1, 0, RETURN_A
-
-    ble a0, a1, ELSE_B
-    sub a0, a0, a1
-
-ELSE_B:
+WHILE_LOOP:
+    beq a1, x0, EXIT
+    bgt a0, a1, GREATER_A
     sub a1, a1, a0
-
-RETURN_A:
+    j WHILE_LOOP
     
+GREATER_A:
+    sub a0, a0, a1
+    j WHILE_LOOP
+    
+EXIT:
     lw ra, 0(sp)
-    addi sp, sp, 8
+    addi sp, sp, 12
     jalr x0, 0(ra)
 
+RETURN_B:
+    addi a0, a1, 0
 ##############################################################
 # PROCEDURE - relPrime(n)
 ##############################################################
 relPrime:
-    #TODO: implement this procedure here
+    addi sp, sp, -12
+    sw ra, 0(sp)
+    sw a0, 4(sp) #a0 = n
+    addi t0, x0, 2 #t0 = m = 2
+    sw t0, 8(sp) #store on stack 
+    
+    WHILE:
+    lw a0, 4(sp) #a0 = n
+    lw a1, 8(sp) #a1 = m
+    jal ra, gcd #call gcd(n, m)
+    
+    addi t0, x0, 1 # t0 = 1
+    beq a0, t0, DONE #if gcd == 1, return m 
+    
+    lw t1, 8(sp) #t1 = m
+    addi t1, t1, 1 #m += 1
+    sw t1, 8(sp) # t1 = m + 1
+    jal x0, WHILE
+    
+    DONE:
+    lw a0, 8(sp) #a0 = m
+    lw ra, 0(sp)
+    addi sp, sp, 12
+    jalr x0, 0(ra)
